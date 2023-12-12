8c2d871df8b197c2a0ad10730c68f6b91fc2d5b1
Varshitha-005 <153415307+Varshitha-005@users.noreply.github.com>
2023-12-12 05:35:52 +0530

    Add files via upload

---
+++ SIMPLE CALCI.py
@@ -0,0 +1,36 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+# Get user input
+val = True
+while val:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operation = input("Enter operation (+, -, *, /): ")
+
+    # Perform calculation based on user's choice
+    if operation == "+":
+        result = add(num1, num2)
+    elif operation == "-":
+        result = subtract(num1, num2)
+    elif operation == "*":
+        result = multiply(num1, num2)
+    elif operation == "/":
+        result = divide(num1, num2)
+    else:
+        result = "Invalid operation"
+        val = False
+
+    print("Result:", result)
